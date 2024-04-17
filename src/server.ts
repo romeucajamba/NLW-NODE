f6779dffdeb2a6be21b7ed5f5ffcc3531f780967
romeucajamba <romeucajamba@gmail.com>
2024-04-17 12:05:09 +0100

    feat:Createad attendees rote

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { createEvents } from './routes/create_events';
+import { registerForEvent } from './routes/register_for_event'
 
 const server = fastify()
 
@@ -8,6 +9,7 @@ server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
 
 server.register(createEvents)
+server.register(registerForEvent)
 
 server.listen({
     port: 3000
